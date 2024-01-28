a49705ff11a47749d6b05f0c78d2b58a18ec9594
Dhara <illuviawriter@gmail.com>
2024-01-27 19:57:24 -0800

    add setup file

---
+++ setup.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+
+// define chess pieces 
+const (
+	E = iota // empty
+	WP // white pawn
+	WR // white rook
+	WN // white knight
+	WB // white bishop
+	WQ // white queen
+	WK // white king
+	BP // black pawn
+	BR // black rook
+	BN // black knight
+	BB // black bishop
+	BQ // black queen
+	BK // black king
+)
+
+// define a 2D array representing the initial chessboard
+initialBoard = [][]int{
+	{WR, WN, WB, WQ, WK, WB, WN, WR},
+    {WP, WP, WP, WP, WP, WP, WP, WP},
+    {E, E, E, E, E, E, E, E},
+    {E, E, E, E, E, E, E, E},
+    {E, E, E, E, E, E, E, E},
+    {E, E, E, E, E, E, E, E},
+    {BP, BP, BP, BP, BP, BP, BP, BP},
+    {BR, BN, BB, BQ, BK, BB, BN, BR},
+}
+
+// Print the initial chessboard
+func PrintBoard() {
+for _, row := range initial_board {
+	for _, piece := range row {
+		fmt.Printf("%2d ", piece)
+	}
+	fmt.Println()
+}
+}
