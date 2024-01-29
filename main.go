3858caf24ac67b45f5c2bb5f82801255c19cb2d4
Dhara <illuviawriter@gmail.com>
2024-01-28 18:53:19 -0800

    add setup structure

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import "fmt"
 
+// define users in two-player game (temporary)
+const userA = "Yin"
+const userB = "Yang"
+
+// define checkmate, stalemate, and quit status
+var checkmateStatus = false
+var stalemateStatus = false
+var userResignedStatus = false
+
 // define chess pieces
 const (
-	E  = "E"  // empty
+	EM = "EM" // empty
 	WP = "WP" // white pawn
 	WR = "WR" // white rook
 	WN = "WN" // white knight
@@ -19,14 +28,26 @@ const (
 	BK = "BK" // black king
 )
 
+// define mapping between letter and column
+const (
+	A = iota
+	B
+	C
+	D
+	E
+	F
+	G
+	H
+)
+
 // define a 2D array representation of the (initial) chessboard
 var board = [8][8]string{
 	{WR, WN, WB, WQ, WK, WB, WN, WR},
 	{WP, WP, WP, WP, WP, WP, WP, WP},
-	{E, E, E, E, E, E, E, E},
-	{E, E, E, E, E, E, E, E},
-	{E, E, E, E, E, E, E, E},
-	{E, E, E, E, E, E, E, E},
+	{EM, EM, EM, EM, EM, EM, EM, EM},
+	{EM, EM, EM, EM, EM, EM, EM, EM},
+	{EM, EM, EM, EM, EM, EM, EM, EM},
+	{EM, EM, EM, EM, EM, EM, EM, EM},
 	{BP, BP, BP, BP, BP, BP, BP, BP},
 	{BR, BN, BB, BQ, BK, BB, BN, BR},
 }
@@ -36,12 +57,14 @@ func PrintBoard() {
 	for _, row := range board {
 		for _, piece := range row {
 			fmt.Printf("%v ", piece)
-			if piece == E {
-				fmt.Print(" ")
-			}
 		}
 		fmt.Println()
 	}
+}
+
+// randomly assign each user either white or black
+func AssignColours() {
+	// do stuff here
 }
 
 // var startPos string
@@ -81,6 +104,11 @@ func PrintBoard() {
 // }
 
 func main() {
-	PrintBoard()
-	// MakeMove()
+	AssignColours()
+	// loop repeatedly until checkmate, stalemate or user resignation
+	for {
+		// if end condition met, exit loop & game
+		// else: alternate userA and userB MakeMove
+		PrintBoard()
+	}
 }
