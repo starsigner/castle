582ca4e7dc14154c12d058cc0a5054418ec1c1a0
Dhara <illuviawriter@gmail.com>
2024-01-28 16:09:58 -0800

    convert array to fixed-size and comment out temp code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module castling
+
+go 1.21.6
--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
- 
+
 import "fmt"
 
-// define chess pieces 
+// define chess pieces
 const (
-	E = "E" // empty
-	WP = "WP"  // white pawn
-	WR = "WR"  // white rook
+	E  = "E"  // empty
+	WP = "WP" // white pawn
+	WR = "WR" // white rook
 	WN = "WN" // white knight
 	WB = "WB" // white bishop
 	WQ = "WQ" // white queen
@@ -19,70 +19,66 @@ const (
 	BK = "BK" // black king
 )
 
-// define a 2D array representing the initial chessboard
-var initialBoard = [][]string{
-    {WR, WN, WB, WQ, WK, WB, WN, WR},
-    {WP, WP, WP, WP, WP, WP, WP, WP},
-    {E, E, E, E, E, E, E, E},
-    {E, E, E, E, E, E, E, E},
-    {E, E, E, E, E, E, E, E},
-    {E, E, E, E, E, E, E, E},
-    {BP, BP, BP, BP, BP, BP, BP, BP},
-    {BR, BN, BB, BQ, BK, BB, BN, BR},
+// define a 2D array representing the (initial) chessboard
+var board = [8][8]string{
+	{WR, WN, WB, WQ, WK, WB, WN, WR},
+	{WP, WP, WP, WP, WP, WP, WP, WP},
+	{E, E, E, E, E, E, E, E},
+	{E, E, E, E, E, E, E, E},
+	{E, E, E, E, E, E, E, E},
+	{E, E, E, E, E, E, E, E},
+	{BP, BP, BP, BP, BP, BP, BP, BP},
+	{BR, BN, BB, BQ, BK, BB, BN, BR},
 }
 
-// Print the initial chessboard
-func PrintBoard() {
-    for _, row := range initialBoard {
-        for _, piece := range row {
-            fmt.Printf("%s ", piece)
-        }
-        fmt.Println()
-    }
-}
+// // Print the initial chessboard
+// func PrintBoard() {
+// 	for _, row := range initialBoard {
+// 		for _, piece := range row {
+// 			fmt.Printf("%v ", piece)
+// 		}
+// 		fmt.Println()
+// 	}
+// }
 
-var startPos string 
-var endPos string
+// var startPos string
+// var endPos string
 
-func MakeMove() {
+// func MakeMove() {
 
-    // get move from user 
-    fmt.Print("Which piece to move: ")
-    fmt.Scanln(&startPos)
-    fmt.Print("Where to move: ")
-    fmt.Scanln(&endPos)
+// 	// get move from user
+// 	fmt.Print("Which piece to move: ")
+// 	fmt.Scanln(&startPos)
+// 	fmt.Print("Where to move: ")
+// 	fmt.Scanln(&endPos)
 
-    // get X and Y coordinates (startPos)
-    startPos_split := []rune(startPos)
-    X_coord_start := startPos_split[0]
-    Y_coord_start := startPos_split[1]
-    fmt.Print(X_coord_start)
-    fmt.Print(Y_coord_start)
+// 	// get X and Y coordinates (startPos)
+// 	startPos_split := []rune(startPos)
+// 	X_coord_start := startPos_split[0]
+// 	Y_coord_start := startPos_split[1]
+// 	fmt.Print(X_coord_start)
+// 	fmt.Print(Y_coord_start)
 
-    // get piece from startPos
-    var piece string
+// 	// get piece from startPos
+// 	var piece string
 
-    if X_coord_start == 'E' {
-        X_coord_start = 4 
-    }
+// 	if X_coord_start == 'E' {
+// 		X_coord_start = 4
+// 	}
 
-    piece = initialBoard[X_coord_start][Y_coord_start]
-    fmt.Print(piece)
+// 	piece = initialBoard[X_coord_start][Y_coord_start]
+// 	fmt.Print(piece)
 
-    // get X and Y coordinates (endPos)
-    endPos_split := []rune(endPos)
-    X_coord_end:= endPos_split[0]
-    Y_coord_end := endPos_split[1]
-    fmt.Print(X_coord_end)
-    fmt.Print(Y_coord_end)
-}
+// 	// get X and Y coordinates (endPos)
+// 	endPos_split := []rune(endPos)
+// 	X_coord_end := endPos_split[0]
+// 	Y_coord_end := endPos_split[1]
+// 	fmt.Print(X_coord_end)
+// 	fmt.Print(Y_coord_end)
+// }
 
 func main() {
-    PrintBoard()
-    MakeMove()
+	//PrintBoard()
+	fmt.Println("hi")
+	// MakeMove()
 }
-
-
-
-
-
